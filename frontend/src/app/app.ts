c7ae50b292a5dc200caa159e7ebb6648145bddf0
sistema-autos-docker <vizcayaarevalo@gmail.com>
2026-09-02 20:21:17 +0000

    Integración exitosa: Angular consumiendo API de Spring Boot con Signals

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent implements OnInit {
+  mensajeBackend = signal<string>('Cargando datos del backend...');
+  private http = inject(HttpClient);
+
+  ngOnInit(): void {
+    const urlBackend = 'https://effective-bassoon-4jxw496rj4xxf7rq6-8080.app.github.dev/api/saludo';
+
+    this.http.get<{ mensaje: string }>(urlBackend).subscribe({
+      next: (data) => this.mensajeBackend.set(data.mensaje),
+      error: (err) => {
+        console.error('Error detallado:', err);
+        this.mensajeBackend.set('Error al conectar con Spring Boot');
+      }
+    });
+  }
 }
+
+
+
+
+
